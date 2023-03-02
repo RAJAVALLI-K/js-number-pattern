38ef99a2db571659215cec0779d891a994f6813b
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-02 22:12:28 +0530

    js-->number-pattern

---
+++ index.js
@@ -0,0 +1,17 @@
+
+for (let i = 5; i >= 1; i--) {
+    for (let j = 1; j <= i; j++) {
+        document.write(j);
+    }
+    document.write("<br>");
+}
+
+
+// pattern
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        document.write(i);
+    }
+    document.write("<br>");
+}
